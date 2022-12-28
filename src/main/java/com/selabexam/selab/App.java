fc2cef6c323e6a15fe980a0702a9f45847fd847c
unknown <chittimelliruchika@gmail.com>
2022-12-28 19:08:31 +0530

    commited

---
+++ src/main/java/com/selabexam/selab/App.java
@@ -0,0 +1,13 @@
+package com.selabexam.selab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
